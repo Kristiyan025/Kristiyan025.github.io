6c0d34767d5ba445c91f4194b57a3efe93c92285
Kristiyan Ivanov Garchev <kristyangarchev@gmail.com>
2022-02-23 15:53:12 +0200

    Working on Projects Page
    Working on Projects Page

---
+++ js/projects.js
@@ -0,0 +1,36 @@
+function hover(e) {
+    let src = e.getAttribute('src');
+    src = src.substring(0, src.length - 3) + 'gif';
+    e.setAttribute('src', src);
+}
+
+function unhover(e) {
+    let src = e.getAttribute('src');
+    src = src.substring(0, src.length - 3) + 'png';
+    e.setAttribute('src', src);
+}
+
+function click(e) {
+    let src = e.getAttribute('src');
+    src = src.substring(0, src.length - 3) + 'mp4';
+}
+
+const container = document.querySelectorAll('.projects-grid')[0];
+
+container.addEventListener('mouseover', e => {
+    if(e.target.localName === 'img'){
+        hover(e.target);
+    }
+});
+
+container.addEventListener('mouseout', e => {
+    if(e.target.localName === 'img'){
+        unhover(e.target);
+    }
+});
+
+container.addEventListener('click', e => {
+    if(e.target.localName === 'img'){
+        click(e.target);
+    }
+});
