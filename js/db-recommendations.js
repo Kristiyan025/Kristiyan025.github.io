602b5da1366b0a1a92a30a5b6c839322e11f2d43
Kristiyan Ivanov Garchev <kristyangarchev@gmail.com>
2026-06-15 23:45:11 +0300

    Automated the creation of the Recommendations page

---
+++ js/db-recommendations.js
@@ -0,0 +1,46 @@
+const work_recommendations = [
+  {
+    name: "Trading 212",
+    namehref: "https://www.trading212.com/",
+    filename: "Trading212.pdf",
+    title: "Trading 212's Recommendation",
+  },
+];
+
+const school_recommendations = [
+  {
+    name: "Assoc. Prof. Asen Bozhilov - Vice Dean at university",
+    filename: "Asen Bozhilov.pdf",
+    title: "Asen Bozhilov's Recommendation",
+  },
+  {
+    name: "Assoc. Prof. Minko Markov - Teacher at university",
+    filename: "Minko Markov.pdf",
+    title: "Minko Markov's Recommendation",
+  },
+  {
+    name: "Yordan Cvetanov - Student counsellor at high school",
+    filename: "Yordan Cvetanov.pdf",
+    title: "Yordan Cvetanov's Recommendation",
+  },
+  {
+    name: "Daniela Zhekova - Math teacher at high school",
+    filename: "Daniela Zhekova.pdf",
+    title: "Daniela Zhekova's Recommendation",
+  },
+  {
+    name: "Keranka Ivanova - Math teacher at high school",
+    filename: "Keranka Ivanova.pdf",
+    title: "Keranka Ivanova's Recommendation",
+  },
+  {
+    name: "Maria Cholakova - Literature teacher at high school",
+    filename: "Maria Cholakova.pdf",
+    title: "Maria Cholakova's Recommendation",
+  },
+  {
+    name: "Veselinka Boyanova - Tambura music teacher at Arts Center, Varosha, Blagoevgrad",
+    filename: "Veselinka Boyanova.pdf",
+    title: "Veselinka Boyanova's Recommendation",
+  },
+];
---
+++ js/recommendations-adder.js
@@ -0,0 +1,23 @@
+function addRecommendation(el, recommendations) {
+  for (const recommendation of recommendations) {
+    console.log(recommendation);
+    let childElement = document.createElement("li");
+    childElement.classList.add("list-item", "event-info", "reveal");
+    childElement.innerHTML = `
+            <a class="name-link" ${recommendation.namehref ? `href="${recommendation.namehref}"` : ""}>
+                <h3 class="event-name">${recommendation.name}</h3>
+            </a>
+            <embed class="pdf" src="./images/recommendations/${recommendation.filename}" 
+                type="application/pdf" title="${recommendation.title}" />
+        `;
+    el.appendChild(childElement);
+  }
+}
+
+let workRecommendationsList = document.getElementById("work-recommendations");
+let schoolRecommendationsList = document.getElementById(
+  "school-recommendations",
+);
+
+addRecommendation(workRecommendationsList, work_recommendations);
+addRecommendation(schoolRecommendationsList, school_recommendations);
